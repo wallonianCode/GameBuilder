c392c21f315ab1cede08672bf3c96103af397ce1
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-19 13:31:21 +0100

    Adaptation of shallow water texture to declined texture model

--- version2/Source/ShallowWater.cpp
+++ version2/Source/ShallowWater.cpp
@@ -1,39 +1,49 @@
 #include "../Headers/ShallowWater.hpp"
 
 
-void ShallowWater::save(std::ostream& os) {
-	os << "ShallowWater" << " ";
-	os << side_.to_str() << " ";
-	Texture::save(os);
-}
-
-
-ShallowWater::ShallowWater(const SDL_Point& posOnScreen) :
-Water(posOnScreen) {
-	init_src();
-}
-
-
-ShallowWater::ShallowWater(const SDL_Point& posOnScreen, 
-const LeafGreenTexture::Side& side) : 
-Water(posOnScreen, side){
-	init_src(side);
-}
-
-
-ShallowWater::ShallowWater(const SDL_Point& posOnScreen,
-const std::string& subType) : ShallowWater(posOnScreen, 
-LeafGreenTexture::Side::to_side(subType)) {}
-	
-
-void ShallowWater::init_src() {
-	this->set_pos_on_tileset({11*S+2, S+2});
-	this->set_dim_on_tileset(S-3, S-3);
-}
-	
-
-void ShallowWater::init_src(const LeafGreenTexture::Side& side) {
-	SDL_Rect srcRect = rectAr[side.to_int()];
-	this->set_pos_on_tileset({srcRect.x, srcRect.y});
-	this->set_dim_on_tileset(srcRect.w, srcRect.h);
+ShallowWater::ShallowWater(const SDL_Point& pos,
+						   const DeclinedTextureOrientation& orientation) {
+	SDL_Rect tile;
+	switch (orientation) {
+		case DeclinedTextureOrientation::North {
+			tile = {11*S+2, 1, S-3, S-3}; 
+			break;
+		}
+		case DeclinedTextureOrientation::West {
+			tile = {10*S+1, S+2, S-3, S-3};
+			break;
+		}
+		case DeclinedTextureOrientation::East {
+			tile = {12*S+2, S+2, S-2, S-3};
+			break;
+		}
+		case DeclinedTextureOrientation::South {
+			tile = {11*S+2, 2*S+2, S-3, S-2};
+			break;
+		}
+		case DeclinedTextureOrientation::NorthWest {
+			tile = {10*S+1, 1, S-3, S-3};
+			break;
+		}
+		case DeclinedTextureOrientation::NorthEast {
+			tile = {12*S+2, 1, S-2, S-3};
+			break;
+		}
+		case DeclinedTextureOrientation::SouthWest {
+			tile = {10*S+1, 1, S-3, S-3};
+			break;
+		}
+		case DeclinedTextureOrientation::SouthEast {
+			tile = {12*S+2, 2*S+2, S-2, S-2};
+			break;
+		}
+		case DeclinedTextureOrientation::Center {
+			tile = {11*S+2, S+2, S-3, S-3};
+			break;
+		}
+		default: 
+			break;
+	}
+	this->set_pos_on_tileset(tile.x, tile.y);
+	this->set_dim_on_tileset(tile.w, tile.h);
 }
