f0e81363ced63d1559130e867d674be4ba9d2e01
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-19 11:18:37 +0100

    Comments deleted from SandRoads header

--- version2/Headers/SandRoad.hpp
+++ version2/Headers/SandRoad.hpp
@@ -3,25 +3,6 @@
 
 #include "Road.hpp"
 
-/*
-class SandRoad : public TextureCloneable<Road, SandRoad> {
-public:
-	SandRoad(const SDL_Point&);
-	SandRoad(const SDL_Point&, const LeafGreenTexture::Side&);
-	SandRoad(const SDL_Point&, const std::string&);
-private:
-	void init_src() override;
-	void init_src(const LeafGreenTexture::Side&) override;
-
-	SDL_Rect arSrcRect[8] = 
-	{ {S+2, 2, S-3, S-3}, {2*S+2, 2, S-3, S-3}, 
-		{3*S+2, 2, S-3, S-3}, {4*S+2, 2, S-3, S-3},
-		{3*S+2, S+2, S-3, S-3}, {4*S+2, S+2, S-3, S-3},
-		{S+2, S+2, S-3, S-3}, {2*S+2, S+2, S-3, S-3} };
-	LeafGreenTexture::Side side_;
-};
-*/
-
 class SandRoad : public TextureCloneable<DeclinedTexture, SandRoad> {
 public:
 	SandRoad(const SDL_Point&, const DeclinedTextureOrientation&);
