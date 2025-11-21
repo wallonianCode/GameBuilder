ab2aaac366bc663ad3c1e8609d60c895d2d84c6e
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-21 15:20:10 +0100

    Adaptation of the selector class to the new architecture

--- version2/Headers/Selector.hpp
+++ version2/Headers/Selector.hpp
@@ -10,29 +10,32 @@
 #include "Texture.hpp"
 
 
-class Selector : Drawable {
+class Selector : public Drawable {
 public:
 	void draw();
 	void handle_event(SDL_Event& event);
 	bool is_mouse_in();
-	bool get_layer_id();
+	// bool get_layer_id();
 	Texture* get_selected_texture();
-	std::vector<Texture*> get_selected_textures();
-	Selector();
 	Selector(const int width);
-	void init();
+//	void init();
 	void switch_layer();
 	/**
 		* @brief  adapt the dimensions of the frame to the texture
 		*				  the mouse is currently hovering over.
 		*/
-	void redimension_frame();
+	//void redimension_frame();
 private:	
+//TODO clean this up
+	LayerManager* layerManager_;
+	Texture* selectedTextureCopy_;
+/*
 	unsigned width_, layerId_;
 	std::array<Layer*, 3> aLayers_;
 	Frame frame_;
 	enum class LayerName {Land, BigLand, Character};
 	LayerName currentLayerName_;
+	*/
 };
 
 #endif
--- version2/Source/Selector.cpp
+++ version2/Source/Selector.cpp
@@ -1,13 +1,23 @@
 #include "../Headers/Selector.hpp"
 
 void Selector::draw() {
-	aLayers_[layerId_]->draw();
+	//aLayers_[layerId_]->draw();
+	if (selectedTextureCopy_ != nullptr) {
+		selectedTextureCopy_ -> draw_shadow();
+	}
+	if (layerManager_ != nullptr) {
+		layerManager_ -> draw();
+	}
+	else {
+		//TODO error management
+	}
 }
 
 //TODO handle event in the selector, not in the layer!
 // The layer is only there for presentation purposes
 void Selector::handle_event(SDL_Event& event) {
 //	aLayers_[layerId_]->handle_event(event);
+/*
 	switch (event.type) {  			
 		case SDL_MOUSEBUTTONDOWN:
 			if (event.button.button == SDL_BUTTON_LEFT) {
@@ -25,6 +35,29 @@ void Selector::handle_event(SDL_Event& event) {
 		default:
 			break;
 	}
+*/
+	switch (event.type) {
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_I) {
+				switch_layer_forward();
+			}
+			else if (event.key.keysym.sym == SDLK_P) {
+				switch_layer_backward();
+			}
+			break;
+		case SDL_MOUSEBUTTONEVENT: {
+			if (event.button.button == SDL_BUTTON_LEFT) {
+				selectedTextureCopy_ = layerManager_->get_selected_texture();
+			}
+			else if (event.button.button == SDL_BUTTON_RIGHT) {
+				selectedTextureCopy_ = nullptr;
+			}
+			break;
+		}
+		default:
+			break;
+	}
+	layerManager_->handle_event(event);
 }
 
 
@@ -41,6 +74,8 @@ bool Selector::get_layer_id() {
 
 
 Texture* Selector::get_selected_texture() {
+	return layerManager_->get_selected_texture();
+	/*
 	Texture* selectedTile = nullptr;
 	try {
 		selectedTile = 
@@ -50,97 +85,50 @@ Texture* Selector::get_selected_texture() {
 		std::cout << error.what() << std::endl;
 		return nullptr;
 	}
-	std::cout << "Selector::get_selected_texture() is not null: " << 
-	(selectedTile != nullptr) << std::endl;
 	return selectedTile;
+	*/
+	
 }
 
 
-std::vector<Texture*> Selector::get_selected_textures() {
-	std::vector<Texture*> vTextures;
-	if (layerId_ == 1) {//big land
-		try {
-			vTextures = aLayers_[layerId_]->get_selected_textures();	
-		}
-		catch (std::runtime_error& error) {
-			std::cout << error.what() << std::endl;
-		}
-	}
-	return vTextures;
-}
 
-
-Selector::Selector() {
-	init();
-}
 
 
 Selector::Selector(const int width) : width_(width) {
-	init();
+	layerManager_ = new LayerManager(width);
 }
 
-
+/*
 void Selector::init() {
 	aLayers_ = { new LandLayer(width_),
-							 new BigLandLayer(width_),
-							 new CharacterLayer(width_)};
+				 new BigLandLayer(width_),
+				 new CharacterLayer(width_)};
 	layerId_ = 0;
 }
+*/
 
-
-void Selector::switch_layer() {
+void Selector::switch_layer_forward() {
+	/*
 	if (layerId_ >= aLayers_.size()-1) 
 		layerId_ = 0;
 	else 
 		layerId_++;
 	std::cout << "Layer id: " << layerId_ << std::endl;
+	*/
+	layerManager_++;
+}
+
+void Selector::switch_layer_backward() {
+	layerManager_--;
 }
 
 
 //TODO call this in the game loop
+/*
 void Selector::redimension_frame() {
-	frame_.set_width(aLayers_[layerId_]->get_texture_width());
-	frame_.set_height(aLayers_[layerId_]->get_texture_height());
+  frame_.set_width(aLayers_[layerId_]->get_texture_width());
+  frame_.set_height(aLayers_[layerId_]->get_texture_height());
   frame_.set_position_on_screen(aLayers_[layerId_])->
 	get_texture_coordinates();
 }
-
-
-/*
-void Selector::switch_layer() {
-	std::array<Layer*, 3>::iterator itCurrentLayer =
-	std::find(aLayers_.begin(), aLayers_.end(), layer_);
-//TODO clear the renderer 	
-	if (itCurrentLayer == aLayers_.end()) {
-		std::cout << "Current layer not in internal representation" <<
-		std::endl;
-  }
-	else if ((itCurrentLayer + 1) == aLayers_.end()) {
-		layer_->reset(aLayers_[0]); 	
-	}
-	else {
-		layer_->reset(*(itCurrentLayer + 1));
-	}
-}
-*/
-
-
-/*
-void Selector::switch_layer() {
-	switch (currentLayerName_) {
-		case LayerName::Land:
-			layer_ = new BigLandLayer(width_);
-			currentLayerName_ = LayerName::BigLand;
-			break;
-		case LayerName::BigLand:
-			layer_ = new CharacterLayer(width_);
-			currentLayerName_ = LayerName::Character;
-			break;
-		case LayerName::Character:
-			layer_ = new LandLayer(width_);
-			currentLayerName_ = LayerName::Land;
-			break;
-		default:
-			break;
-	}
-*/
+	*/
