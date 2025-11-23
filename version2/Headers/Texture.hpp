f9b427243111bf1863efffafb3e275b5144d0321
wallonianCode <49552648+wallonianCode@users.noreply.github.com>
2025-11-23 09:59:14 +0100

    get_pos_on_screen() method -> get_upper_left_corner'

--- version2/Headers/Texture.hpp
+++ version2/Headers/Texture.hpp
@@ -30,12 +30,11 @@ public:
 	virtual void update()	= 0;
 	virtual void save(std::ostream&) const = 0;
 	virtual Texture* clone() const = 0;
-	virtual SDL_Point get_pos_on_screen() const = 0;
+	virtual SDL_Point get_upper_left_corner() const = 0;
 	virtual void set_pos_on_screen(const SDL_Point&) = 0;
 	//TODO implement
 	virtual void get_width() const = 0;
 	virtual void get_height() const = 0;
-
 private:
 	std::string tileset_;		
 };
