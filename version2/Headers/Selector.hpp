e7d02b6200c63024ec670775a06f3a30590d4581
wallonianCode <49552648+wallonianCode@users.noreply.github.com>
2025-11-23 09:57:09 +0100

    Deleted comment. Reintroduced frame manipulation

--- version2/Headers/Selector.hpp
+++ version2/Headers/Selector.hpp
@@ -15,27 +15,17 @@ public:
 	void draw();
 	void handle_event(SDL_Event& event);
 	bool is_mouse_in();
-	// bool get_layer_id();
 	Texture* get_selected_texture();
 	Selector(const int width);
-//	void init();
-	void switch_layer();
+	void switch_layer_forward();
+	void switch_layer_backward();
 	/**
 		* @brief  adapt the dimensions of the frame to the texture
 		*				  the mouse is currently hovering over.
 		*/
-	//void redimension_frame();
+	void redimension_frame();
 private:	
-//TODO clean this up
 	LayerManager* layerManager_;
-	Texture* selectedTextureCopy_;
-/*
-	unsigned width_, layerId_;
-	std::array<Layer*, 3> aLayers_;
-	Frame frame_;
-	enum class LayerName {Land, BigLand, Character};
-	LayerName currentLayerName_;
-	*/
 };
 
 #endif
--- version2/Source/Selector.cpp
+++ version2/Source/Selector.cpp
@@ -1,41 +1,18 @@
 #include "../Headers/Selector.hpp"
 
 void Selector::draw() {
-	//aLayers_[layerId_]->draw();
+	Texture* selectedTextureCopy;
+
+	selectedTextureCopy = 
+	layerManager_->get_texture_copy_at_coord(frame_->get_coord());
 	if (selectedTextureCopy_ != nullptr) {
-		selectedTextureCopy_ -> draw_shadow();
+		selectedTextureCopy -> draw_shadow();
 	}
-	if (layerManager_ != nullptr) {
-		layerManager_ -> draw();
-	}
-	else {
-		//TODO error management
-	}
+	layerManager_ -> draw();
 }
 
-//TODO handle event in the selector, not in the layer!
-// The layer is only there for presentation purposes
+
 void Selector::handle_event(SDL_Event& event) {
-//	aLayers_[layerId_]->handle_event(event);
-/*
-	switch (event.type) {  			
-		case SDL_MOUSEBUTTONDOWN:
-			if (event.button.button == SDL_BUTTON_LEFT) {
-				//this->select_texture();			
-			  frame_->immobilize();	
-			}
-			else if (event.button.button == SDL_BUTTON_RIGHT) {
-			  frame_->free();	
-			}
-			break;
-		case SDL_MOUSEMOTION: 
-			frame_->follow_mouse_motion();
-			this->redimension_frame();
-			break;
-		default:
-			break;
-	}
-*/
 	switch (event.type) {
 		case SDL_KEYDOWN:
 			if (event.key.keysym.sym == SDLK_I) {
@@ -47,13 +24,18 @@ void Selector::handle_event(SDL_Event& event) {
 			break;
 		case SDL_MOUSEBUTTONEVENT: {
 			if (event.button.button == SDL_BUTTON_LEFT) {
-				selectedTextureCopy_ = layerManager_->get_selected_texture();
+				frame_ -> immobilize();
 			}
 			else if (event.button.button == SDL_BUTTON_RIGHT) {
-				selectedTextureCopy_ = nullptr;
+				frame_ -> free();
 			}
 			break;
 		}
+		case SDL_MOUSEMOTION: {
+			frame_ -> update(this->get_current_texture_rectangle());
+			break;
+		}
+
 		default:
 			break;
 	}
@@ -68,67 +50,34 @@ bool Selector::is_mouse_in() {
 }
 
 
-bool Selector::get_layer_id() {
-	return layerId_;
+
+
+Texture* Selector::get_texture_copy_at_coord() {
+	return layerManager_->get_texture_copy_at_coord(frame_->get_coord());	
 }
-
-
-Texture* Selector::get_selected_texture() {
-	return layerManager_->get_selected_texture();
-	/*
-	Texture* selectedTile = nullptr;
-	try {
-		selectedTile = 
-		aLayers_[layerId_]->get_texture_at_coord(frame_->get_position());
-	}
-	catch (std::runtime_error& error){
-		std::cout << error.what() << std::endl;
-		return nullptr;
-	}
-	return selectedTile;
-	*/
-	
-}
-
-
-
 
 
 Selector::Selector(const int width) : width_(width) {
 	layerManager_ = new LayerManager(width);
 }
 
-/*
-void Selector::init() {
-	aLayers_ = { new LandLayer(width_),
-				 new BigLandLayer(width_),
-				 new CharacterLayer(width_)};
-	layerId_ = 0;
-}
-*/
 
 void Selector::switch_layer_forward() {
-	/*
-	if (layerId_ >= aLayers_.size()-1) 
-		layerId_ = 0;
-	else 
-		layerId_++;
-	std::cout << "Layer id: " << layerId_ << std::endl;
-	*/
-	layerManager_++;
+	++layerManager_;
 }
 
 void Selector::switch_layer_backward() {
-	layerManager_--;
+	--layerManager_;
 }
 
 
-//TODO call this in the game loop
-/*
 void Selector::redimension_frame() {
-  frame_.set_width(aLayers_[layerId_]->get_texture_width());
-  frame_.set_height(aLayers_[layerId_]->get_texture_height());
-  frame_.set_position_on_screen(aLayers_[layerId_])->
-	get_texture_coordinates();
+	Texture* textureAtCoord;
+
+	textureAtCoord = 
+	layerManager_->get_texture_copy_at_coord(frame_->get_coord());
+
+	frame_.set_width(textureAtCoord->get_width());
+	frame_.set_heigth(textureAtCoord->get_height());
+	frame_.set_upper_left_corner(textureAtCoord->get_upper_left_corner());
 }
-	*/
