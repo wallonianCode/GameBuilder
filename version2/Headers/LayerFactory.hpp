37043125a66047f00e6b4407a2690de090246aa5
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-24 16:11:35 +0100

    Headers of the created textures added + LayerTypes updated

--- version2/Headers/LayerFactory.hpp
+++ version2/Headers/LayerFactory.hpp
@@ -1,9 +1,18 @@
 #ifndef LAYERFACTORY_H
 #define LAYERFACTORY_H
 
+#include "Layer.hpp"
+#include "RoadLayer.hpp"
+#include "TreeLayer.hpp"
+#include "SoilLayer.hpp"
+#include "BuildingsLayer.hpp"
+#include "WaterLayer.hpp"
+#include "BorderLayer.hpp"
+
+
 class LayerFactory {
 public:
-    enum class LayerType {Roads, Trees, Soil, Buildings, Water};
+    enum class LayerType {Road, Tree, Soil, Buildings, Water, Border};
     static Layer* create_layer(const LayerFactory::LayerType&);
 		LayerType operator++(const LayerType&, int); //postfix
 		LayerType operator++(const LayerType&);
