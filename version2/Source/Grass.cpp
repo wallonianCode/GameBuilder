38c51a4d6461ba271ca987c5795f071fd6a114f2
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-24 16:09:57 +0100

    Grass refactored to new model with factories and enum

--- version2/Source/Grass.cpp
+++ version2/Source/Grass.cpp
@@ -1,62 +1,30 @@
 #include "../Headers/Grass.hpp"
 
 
-//-------------------------------------------------------------------
-//														FORMAT
-//-------------------------------------------------------------------
 
-Grass::Format Grass::Format::to_format(const std::string& str) {
-	auto ptr = std::find(strTypes_.begin(), strTypes_.end(), str);
-	if (ptr == strTypes_.end()) {
-		throw new std::runtime_error("Unknown type: " + str);	
+Grass::Grass(const SDL_Point& pos, const GrassType& grassType) : SimpleTexture(pos) {
+	SDL_Rect tile;
+
+	switch (orientation) {
+		case GrassType::GreenWhite: {
+			tile = {6*S+2, 2, S-3, S-3};
+			break;
+		}
+		case GrassType::GreenWhiteGreen: {
+			tile = {6*S+2, S+2, S-3, S-3};
+			break;
+		}
+		case GrassType::Green: {
+			tile = {7*S+2, S+2, S-3, S-3};
+			break;
+		}
+		case GrassType::White: {
+			tile = {6*S+2, 2*S+2, S-3, S-3};
+			break;
+		}
+		default:
+			break;
 	}
-	int index = ptr - strTypes_.begin();
-	return Format(static_cast<Type>(index));
+	this->set_pos_on_tileset(tile.x, tile.y);
+	this->set_dim_on_tileset(tile.w, tile.h);
 }
-	
-
-std::string Grass::Format::to_str(const Type& type) {
-	return strTypes_[static_cast<int>(type)];
-}
-
-
-SDL_Rect Grass::Format::get_rect() const {
-	return srcRect_[static_cast<int>(type_)];
-}
-
-
-Grass::Format::Format(const Grass::Format::Type& type) : type_(type) {}
-
-
-std::array<std::string, 4> Grass::Format::strTypes_ = 
-{"GreenWhite", "GreenWhiteGreen", "Green", "White"};
-
-//------------------------------------------------------------------
-//														GRASS
-//------------------------------------------------------------------
-Grass::Grass(const SDL_Point& posOnScreen, const Format& format) : 
-LeafGreenTexture(posOnScreen), format_(format) {
-	this->init_src();	
-}
-
-
-Grass::Grass(const SDL_Point& posOnScreen, const std::string& strFormat) :
-Grass(posOnScreen, Grass::Format::to_format(strFormat)) {}
-
-
-void Grass::update() {}
-
-
-void Grass::init_src() {
-	SDL_Rect rect = format_.get_rect();
-	this->set_pos_on_tileset({rect.x, rect.y});
-	this->set_dim_on_tileset(rect.w, rect.h);
-}
-
-/*
-void Grass::init_src(const Grass::Format& format) {
-	SDL_Rect rect = format.get_rect();
-	this->set_pos_on_tileset({rect.x, rect.y});
-	this->set_dim_on_tileset(rect.w, rect.h);
-}
-*/
