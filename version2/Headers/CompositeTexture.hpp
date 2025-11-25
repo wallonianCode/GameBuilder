2f5c85d89dcacb4320d5487b381ae30e1662fdf3
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-25 15:00:21 +0100

    Obsolete comments deleted

--- version2/Headers/CompositeTexture.hpp
+++ version2/Headers/CompositeTexture.hpp
@@ -6,15 +6,6 @@
 
 #include "Texture.hpp"
 
-//TODO replace inner classes with factory methods
-
-
-/// This class is a placeholder for the source and destination rectangles.
-/// These rectangles are necessary parameters for the renderer.
-
-
-//TODO develop a SimpleTexture class to take on the methods that only
-//     make sense for SimpleTextures.
 
 class CompositeTexture : 
 public TextureCloneable<Texture, CompositeTexture> {
@@ -25,16 +16,15 @@ public:
 									 std::vector::iterator<SDL_Rect> itDestBegin,
 									 std::vector::iterator<SDL_Rect> itDestEnd);
 
-  void draw() override;
-  void update() override;
+	void draw() override;
+  	void update() override;
 	void save() override;
 	
-	//TODO save in a database
 	void save_dest(std::ostream&) const override;
 	SDL_Point get_upper_left_corner() const override;
 	void set_pos_on_screen(const SDL_Point&) const override;
 private:
-  std::vector<std::pair<SDL_Rect, SDL_Rect>> vSrcDest_;
+  	std::vector<std::pair<SDL_Rect, SDL_Rect>> vSrcDest_;
 
 };
 
