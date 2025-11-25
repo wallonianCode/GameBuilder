b4309d580b76c20a50f49844d9741a1154604f36
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-25 15:01:01 +0100

    Renaming of variable, call to get_upper_left_corner() instead of get_pos_on_screen()

--- version2/Source/TexturePositionComparator.cpp
+++ version2/Source/TexturePositionComparator.cpp
@@ -1,20 +1,19 @@
 #include "../Headers/TexturePositionComparator.hpp"
 
 
-TexturePositionComparator::TexturePositionComparator(
-const SDL_Point& pos) : posOnScreen_(pos) {}
+TexturePositionComparator::TexturePositionComparator(const SDL_Point& pos) : 
+posOnScreen_(pos) {}
+
 
 bool TexturePositionComparator::operator()(const Texture& texture) {
-	const SDL_Point texturePosOnScreen = texture.get_pos_on_screen();
-	return (texturePosOnScreen.x == posOnScreen_.x &&
-	texturePosOnScreen.y == posOnScreen_.y);
+	const SDL_Point textureUpperLeftCorner = texture.get_upper_left_corner();
+	return (textureUpperLeftCorner.x == posOnScreen_.x &&
+			textureUpperLeftCorner.y == posOnScreen_.y);
 }
 
 
 bool TexturePositionComparator::operator()(Texture* texture) {
-	const SDL_Point texturePosOnScreen = texture->get_pos_on_screen();
-	return (texturePosOnScreen.x == posOnScreen_.x &&
-	texturePosOnScreen.y == posOnScreen_.y);
+	const SDL_Point textureUpperLeftCorner = texture->get_upper_left_corner();
+	return (textureUpperLeftCorner.x == posOnScreen_.x &&
+			textureUpperLeftCorner.y == posOnScreen_.y);
 }
-
-
