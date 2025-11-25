0fb30424080cebd879942a5642b636674f59f54f
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-25 13:39:53 +0100

    Soil layer fully implemented

--- version2/Headers/SoilLayer.hpp
+++ version2/Headers/SoilLayer.hpp
@@ -3,7 +3,7 @@
 
 #include "Layer.hpp"
 
-//TODO build a grass factory
+
 class SoilLayer : public Layer {
 public:
     SoilLayer(const int, const SDL_Point&);
--- version2/Source/SoilLayer.cpp
+++ version2/Source/SoilLayer.cpp
@@ -3,8 +3,36 @@
 
 SoilLayer::SoilLayer(const int width, const SDL_Point& upperLeftCorner) :
 Layer(width, upperLeftCorner) {
+    int i;
+    SDL_Point pos;
+    std::array<SimpleTextureEnum, 4> grassTypes, plants;
     
-    //TODO
+    grassTypes = {SimpleTextureEnum::GreenWhiteGrass, SimpleTextureEnum::GreenWhiteGreenGrass,
+                  SimpleTextureEnum::GreenGrass, SimpleTextureEnum::WhiteGrass};
+    
+    plantTypes = {SimpleTextureEnum::Bush, SimpleTextureEnum::Flowers, 
+                  SimpleTextureEnum::Moos, SimpleTextureEnum::TallGrass};
+    i = 0;
+    pos = upperLeftCorner;
+    
+    for (i = 0; i <= 3; ++i) {
+        posGrass = {upperLeftCorner.x + i*SimpleTexture::get_width(), upperLeftCorner.y};
+        posPlant = {upperLeftCorner.x + i*SimpleTexture::get_width(), 
+                    upperLeftCorner.y + SimpleTexture::get_height()};
+        this->add_texture(TextureFactory::create_simple_texture({posGrass}), grassTypes[i]);
+        this->add_texture(TextureFactory::create_simple_texture({posPlant}), plantTypes[i]);
+        i++;
+    }
+
+
+    this->add_texture(
+        TextureFactory::create_simple_texture({upperLeftCorner.x, 
+                                               upperLeftCorner.y + 2*SimpleTexture::get_height()},
+                                               SimpleTextureEnum::Sand));
+    this->add_texture(
+        TextureFactory::create_simple_texture({upperLeftCorner.x + SimpleTexture::get_width(), 
+                                               upperLeftCorner.y + 2*SimpleTexture::get_height()},
+                                              SimpleTextureEnum::Rock));   
 
 
 }
