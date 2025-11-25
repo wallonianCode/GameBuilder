2b619cd088516f41b03849a87d0af157b66bfa02
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-25 13:00:52 +0100

    Renaming of constructor argument

--- version2/Source/Flowers.cpp
+++ version2/Source/Flowers.cpp
@@ -2,8 +2,8 @@
 
 
 
-Flowers::Flowers(const SDL_Point& posOnScreen) : 
-SimpleTexture(posOnScreen) {
+Flowers::Flowers(const SDL_Point& pos) : 
+SimpleTexture(pos) {
 	this->set_pos_on_tileset({7*S+2, 2*S+2});	
 	this->set_dim_on_tileset(S-3, S-3);
 }
