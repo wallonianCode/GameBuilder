29fbf5ae389b743d3e0db1ef9a68bb5fa0e20f87
Chevalier Maxime <maxime.chevalier@etm.at>
2025-11-27 16:45:45 +0100

    Include statement refactored

--- version2/Headers/CompositeTexture.hpp
+++ version2/Headers/CompositeTexture.hpp
@@ -2,27 +2,25 @@
 #define COMPOSED_TEXTURE
 
 #include <vector>
-#include <pair>
+#include <utility>
 
 #include "Texture.hpp"
+#include "TextureCloneable.hpp"
 
 
 class CompositeTexture : 
 public TextureCloneable<Texture, CompositeTexture> {
 public:
 	CompositeTexture(const std::string& tileset, 
-  								 std::vector::iterator<SDL_Rect> itSrcBegin,
-									 std::vector::iterator<SDL_Rect> itSrcEnd,
-									 std::vector::iterator<SDL_Rect> itDestBegin,
-									 std::vector::iterator<SDL_Rect> itDestEnd);
+  								 std::vector<SDL_Rect>::iterator itSrcBegin,
+									 std::vector<SDL_Rect>::iterator itSrcEnd,
+									 std::vector<SDL_Rect>::iterator itDestBegin,
+									 std::vector<SDL_Rect> ::iterator itDestEnd);
 
 	void draw() override;
   	void update() override;
-	void save() override;
 	
-	void save_dest(std::ostream&) const override;
 	SDL_Point get_upper_left_corner() const override;
-	void set_pos_on_screen(const SDL_Point&) const override;
 private:
   	std::vector<std::pair<SDL_Rect, SDL_Rect>> vSrcDest_;
 
