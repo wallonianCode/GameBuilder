d153e92c086ae188eadb892a63854d3f55db6d33
wallonianCode <49552648+wallonianCode@users.noreply.github.com>
2025-12-24 07:11:13 +0100

    main structure

--- version2/Source/main.cpp
+++ version2/Source/main.cpp
@@ -1,8 +1,33 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+
 #include <iostream>
 #include <exception>
+
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_init.h> //TODO verify if not already included in main
+#include <SDL3/SDL_main.h>
+
 #include "../Headers/GameManager.hpp"
+
+SDL_AppResult SDL_AppInit(void **appState, int argc, char *argv[]) {
+	return SDL_APP_SUCCESS;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event* event) {
+	return SDL_APP_SUCCESS;
+}
+
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+	return SDL_APP_SUCCESS;
+}
+
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+
+}
+
+
 
 
 int main() {
