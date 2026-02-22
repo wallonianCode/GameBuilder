b2740bab028e9ad473adb8ffe15a2a6e73ea1316
wallonianCode <49552648+wallonianCode@users.noreply.github.com>
2026-02-22 07:42:07 +0100

    Composite textures get_entry() method returns pointer on entry representing the upper left corner only.

--- version2/Headers/CompositeTexture.hpp
+++ version2/Headers/CompositeTexture.hpp
@@ -22,6 +22,7 @@ public:
   void update() override;
 	
 	SDL_FPoint get_upper_left_corner() const override;
+
 	void set_upper_left_corner(const SDL_FPoint&) override;
 
 /*
@@ -31,10 +32,8 @@ public:
 	void move(const SDL_FPoint&);
 	virtual bool is_coord_in_texture(const SDL_FPoint&) const override;
 	// get all table entries for a composite texture diplayed on the map.
-	void get_entries(
-	std::vector<TextureTableEntry>::iterator itEntriesBegin,
-	std::vector<TextureTableEntry>::iterator itEntriesEnd) const;
-private:
+	virtual TextureTableEntry* get_entry() const override;
+private:	
   std::vector<std::pair<SDL_FRect, SDL_FRect>> vSrcDest_;
 
 };
